3108b41f77de0ec9cea12b908d7a073b572d1814
Breno Tomé <breno@pop-os.localdomain>
2020-09-06 12:07:15 -0300

    adiciona circulo

--- src/basic.renderer.js
+++ src/basic.renderer.js
@@ -1,96 +1,89 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(global = global || self, factory(global.BasicRenderer = {}));
-}(this, (function (exports) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+      (global = global || self, factory(global.BasicRenderer = {}));
+}(this, (function (exports) {
+  'use strict';
 
 
-        /* ------------------------------------------------------------ */
+  /* ------------------------------------------------------------ */
 
-        
-    function inside(  x, y, shape  ) 
-    {
-        if (!shape.isPointInsideBoundingBox(x,y))
-            return false
-        return shape.isPointInside(x,y);
-    }
-        
-    
-    function Screen( width, height, scene ) {
-        this.width = width;
-        this.height = height;
-        this.scene = this.preprocess(scene);   
-        this.createImage(); 
-    }
 
-    Object.assign( Screen.prototype, {
+  function inside(x, y, shape) {
+    return shape.isPointInside(x, y);
+  }
 
-            preprocess: function(scene) {
-                // cria objeto shape e define boundingBox
-                
-                var preprop_scene = [];
-                
-                for( var primitive of scene ) {  
-                    const createShape = new CreateShape(primitive.shape, primitive.vertices, primitive.color);
-                    const shape = createShape.getInstance();
 
-                    preprop_scene.push( shape );
-                }
+  function Screen(width, height, scene) {
+    this.width = width;
+    this.height = height;
+    this.scene = this.preprocess(scene);
+    this.createImage();
+  }
 
-                return preprop_scene;
-            },
+  Object.assign(Screen.prototype, {
 
-            createImage: function() {
-                this.image = nj.ones([this.height, this.width, 3]).multiply(255);
-            },
+    preprocess: function (scene) {
+      // cria objeto shape e define boundingBox
 
-            rasterize: function() {
-                var color;
-         
-                // In this loop, the image attribute must be updated after the rasterization procedure.
-                for( var primitive of this.scene ) {
+      var preprop_scene = [];
 
-                    // Loop through all pixels
-                    for (var i = 0; i < this.width; i++) {
-                        var x = i + 0.5;
-                        for( var j = 0; j < this.height; j++) {
-                            var y = j + 0.5;
+      for (var primitive of scene) {
+        const createShape = new CreateShape(primitive);
+        const shape = createShape.getInstance();
 
-                            // First, we check if the pixel center is inside the primitive 
-                            if ( inside( x, y, primitive ) ) {
-                                // only solid colors for now
-                                color = primitive.color;
-                                this.set_pixel( i, this.height - (j + 1), color );
-                            }
-                            
-                        }
-                    }
-                }
-                
-               
-              
-            },
+        preprop_scene.push(shape);
+      }
 
-            set_pixel: function( i, j, colorarr ) {
-                // We assume that every shape has solid color
-         
-                this.image.set(j, i, 0,    colorarr.get(0));
-                this.image.set(j, i, 1,    colorarr.get(1));
-                this.image.set(j, i, 2,    colorarr.get(2));
-            },
+      return preprop_scene;
+    },
 
-            update: function () {
-                // Loading HTML element
-                var $image = document.getElementById('raster_image');
-                $image.width = this.width; $image.height = this.height;
+    createImage: function () {
+      this.image = nj.ones([this.height, this.width, 3]).multiply(255);
+    },
 
-                // Saving the image
-                nj.images.save( this.image, $image );
+    rasterize: function () {
+      var color;
+
+      // In this loop, the image attribute must be updated after the rasterization procedure.
+      for (var primitive of this.scene) {
+
+        // Loop through all pixels
+        for (var i = 0; i < this.width; i++) {
+          var x = i + 0.5;
+          for (var j = 0; j < this.height; j++) {
+            var y = j + 0.5;
+            // First, we check if the pixel center is inside the primitive 
+            if (inside(x, y, primitive)) {
+              // only solid colors for now
+              color = primitive.color;
+              this.set_pixel(i, this.height - (j + 1), color);
             }
+          }
         }
-    );
+      }
+    },
 
-    exports.Screen = Screen;
-    
+    set_pixel: function (i, j, colorarr) {
+      // We assume that every shape has solid color
+
+      this.image.set(j, i, 0, colorarr.get(0));
+      this.image.set(j, i, 1, colorarr.get(1));
+      this.image.set(j, i, 2, colorarr.get(2));
+    },
+
+    update: function () {
+      // Loading HTML element
+      var $image = document.getElementById('raster_image');
+      $image.width = this.width; $image.height = this.height;
+
+      // Saving the image
+      nj.images.save(this.image, $image);
+    }
+  }
+  );
+
+  exports.Screen = Screen;
+
 })));
 
--- src/shapes.js
+++ src/shapes.js
@@ -1,55 +1,100 @@
-class CreateShape{//instancia o poligono de acordo com o nome
-    constructor(name, vertices, color){
-        this.name = name
-        this.vertices = vertices
-        this.color = color
+class CreateShape {//instancia o poligono de acordo com o nome
+  constructor(primitive) {
+    this.primitive = primitive
+  }
+  getInstance() {
+    const shapes = {
+      "triangle": Triangle,
+      "circle": Circle,
     }
-    getInstance(){
-        const shapes = {
-            "triangle" : Triangle
-        }
-        return new shapes[this.name](this.name, this.vertices, this.color)
-    }
+    return new shapes[this.primitive.shape](this.primitive);
+  }
 }
 
-class Triangle{
-    constructor(name, vertices, color){
-        this.name = name;
-        this.vertices = vertices;
-        this.color = color;
-        this.boundingBox = this.getBoundingBox();
-    }
-    isPointInside(x,y){
-        /**
-         * Implementa mesma tecnica da lista de exercicios
-         */
-        const point = nj.array([x,y])
-        const combinations = [[0,1],[1,2],[2,0]] //combinações de vertices v2-v1 v3-v2 v1-v3
-        const rotate = nj.array([[0,-1],[1,0]]) //matriz de rotação
-        for (let i = 0; i < 3; i++) {
-            let v1 = this.vertices.pick(combinations[i][0])
-            let v2 = this.vertices.pick(combinations[i][1])
-            let vectorDirector = nj.subtract(v2,v1)
-            let normal = nj.dot(rotate,vectorDirector)
-            let pointVector = nj.subtract(point, v1)
-            let out = nj.dot(normal, pointVector)
-            if (out.get(0) < 1)//caso o angulo não seja agudo o ponto está fora do trinangulo
-                return false
-        }
-        return true
-    }
-    isPointInsideBoundingBox(x,y){
-        //verifica se ponto está dentro do box
-        const insideX = (x >= this.boundingBox.get(0,0)) && (x <= this.boundingBox.get(1,0)) 
-        const insideY = (y >= this.boundingBox.get(0,1)) && (y <= this.boundingBox.get(1,1))
-        return (insideX && insideY)
-    }
-    getBoundingBox(){
-        const minX = this.vertices.pick(null,0).min()
-        const minY = this.vertices.pick(null,1).min()
-        const maxX = this.vertices.pick(null,0).max()
-        const maxY = this.vertices.pick(null,1).max()
-        return nj.array([[minX,minY],[maxX,maxY]])
-    }
 
+class Triangle {
+  constructor(primitive) {
+    this.name = primitive.shape;
+    this.vertices = primitive.vertices;
+    this.color = primitive.color;
+    this.boundingBox = new BoundingBox(this.vertices);
+  }
+  isPointInside(x, y) {
+    /**
+     * Implementa mesma tecnica da lista de exercicios
+     */
+    if (!this.boundingBox.isPointInside(x,y))
+      return false;
+
+    const point = nj.array([x, y]);
+    const combinations = [[0, 1], [1, 2], [2, 0]]; //combinações de vertices v2-v1 v3-v2 v1-v3
+    const rotate = nj.array([[0, -1], [1, 0]]); //matriz de rotação
+    for (let i = 0; i < 3; i++) {
+      let v1 = this.vertices.pick(combinations[i][0]);
+      let v2 = this.vertices.pick(combinations[i][1]);
+      let vectorDirector = nj.subtract(v2, v1);
+      let normal = nj.dot(rotate, vectorDirector);
+      let pointVector = nj.subtract(point, v1);
+      let out = nj.dot(normal, pointVector);
+      if (out.get(0) < 1)//caso o angulo não seja agudo o ponto está fora do trinangulo
+        return false;
+    }
+    return true;
+  }
+}
+
+class Circle {
+  constructor(primitive) {
+    this.name = primitive.shape;
+    this.center = primitive.center;
+    this.radius = primitive.radius;
+    this.color = primitive.color;
+    this.boundingBox = new BoundingBox(primitive, true);
+  }
+  isPointInside(x, y) {
+    if (!this.boundingBox.isPointInside(x,y))
+      return false;
+    const centerX = this.center.get(0);
+    const centerY = this.center.get(1);
+    // fora da equação do circulo
+    const out = Math.pow(x-centerX,2) + Math.pow(y-centerY,2) > Math.pow(this.radius,2)
+    if(out){
+      return false;
+    }
+    return true;
+  }
+}
+
+
+
+class BoundingBox{
+  constructor(primitive, isCircle = false){
+    if (!isCircle)
+      this.boundingBox = this.getBoundingBox(primitive.vertices);
+    this.boundingBox = this.getCircleBoundingBox(primitive);
+  }
+  getBoundingBox(vertices) {
+    const minX = vertices.pick(null, 0).min();
+    const minY = vertices.pick(null, 1).min();
+    const maxX = vertices.pick(null, 0).max();
+    const maxY = vertices.pick(null, 1).max();
+    return nj.array([[minX, minY], [maxX, maxY]]);
+  }
+  getCircleBoundingBox(primitive) {
+    const centerX = primitive.center.get(0);
+    const centerY = primitive.center.get(1);
+    const radius = primitive.radius;
+
+    const minX = centerX - radius;
+    const minY = centerY - radius;
+    const maxX = centerX + radius;
+    const maxY = centerY + radius;
+    return nj.array([[minX, minY], [maxX, maxY]]);
+  }
+  isPointInside(x, y){
+    //verifica se ponto está dentro do box
+    const insideX = (x >= this.boundingBox.get(0, 0)) && (x <= this.boundingBox.get(1, 0));
+    const insideY = (y >= this.boundingBox.get(0, 1)) && (y <= this.boundingBox.get(1, 1));
+    return (insideX && insideY);
+  }
 }
